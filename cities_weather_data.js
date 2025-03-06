5d02a39fd1785f673738c0e686a0e1bdff8d6b2e
shaikmansoor422 <skmansoor422@gmail.com>
2025-03-06 13:18:46 +0530

    "the cirties data api changed to get accurate data"

--- cities_weather_data.js
+++ cities_weather_data.js
@@ -1,7 +1,9 @@
 
     //Fetching the bangalore weather data
     fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f95c5e7704d24f868bc42051250503&q=bangalore`
+      `https://api.open-meteo.com/v1/forecast?latitude=13.0836939&longitude=80.270186&current_weather=true&hourly=relative_humidity_2m,wind_speed_10m,wind_direction_10m
+
+`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -10,19 +12,20 @@
         //adding data to the Weather conditions
         document.getElementById(
           "bang_temp"
-        ).innerHTML = `<strong>${data.current.temp_c}°C</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.temperature}°C</strong>`;
         document.getElementById(
           "bang_humi"
-        ).innerHTML = `<strong>${data.current.humidity}(g/kg)</strong>`;
+        ).innerHTML = `<strong>${data.hourly.relative_humidity_2m[0]}(g/kg)</strong>`;
         document.getElementById(
           "bang_wind"
-        ).innerHTML = `<strong>${data.current.wind_kph}kph</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.windspeed}kph</strong>`;
       })
       .catch((error) => console.error("Error:", error));
   
     //Fetchig the chennai weather data
     fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f95c5e7704d24f868bc42051250503&q=chennai`
+      `https://api.open-meteo.com/v1/forecast?latitude=13.0837&longitude=80.2702&current_weather=true&hourly=relative_humidity_2m,wind_speed_10m,wind_direction_10m
+`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -31,19 +34,21 @@
         //adding data to the Weather conditions
         document.getElementById(
           "chen_temp"
-        ).innerHTML = `<strong>${data.current.temp_c}°C</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.temperature}°C</strong>`;
         document.getElementById(
           "chen_humi"
-        ).innerHTML = `<strong>${data.current.humidity}(g/kg)</strong>`;
+        ).innerHTML = `<strong>${data.hourly.relative_humidity_2m[0]}(g/kg)</strong>`;
         document.getElementById(
           "chen_wind"
-        ).innerHTML = `<strong>${data.current.wind_kph}kph</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.windspeed}kph</strong>`;
       })
       .catch((error) => console.error("Error:", error));
   
     ////Fetchig the mumbai weather data
     fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f95c5e7704d24f868bc42051250503&q=mumbai`
+      `https://api.open-meteo.com/v1/forecast?latitude=19.0760&longitude=72.8777&current_weather=true&hourly=relative_humidity_2m,wind_speed_10m,wind_direction_10m
+
+`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -52,19 +57,21 @@
         //adding data to the Weather conditions
         document.getElementById(
           "mum_temp"
-        ).innerHTML = `<strong>${data.current.temp_c}°C</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.temperature}°C</strong>`;
         document.getElementById(
           "mum_humi"
-        ).innerHTML = `<strong>${data.current.humidity}(g/kg)</strong>`;
+        ).innerHTML = `<strong>${data.hourly.relative_humidity_2m[0]}(g/kg)</strong>`;
         document.getElementById(
           "mum_wind"
-        ).innerHTML = `<strong>${data.current.wind_kph}kph</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.windspeed}kph</strong>`;
       })
       .catch((error) => console.error("Error:", error));
   
     //Fetchig the kolkata weather data
     fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f95c5e7704d24f868bc42051250503&q=kolkata`
+      `https://api.open-meteo.com/v1/forecast?latitude=22.5726&longitude=88.3639&current_weather=true&hourly=relative_humidity_2m,wind_speed_10m,wind_direction_10m
+
+`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -73,13 +80,13 @@
         //adding data to the Weather conditions
         document.getElementById(
           "kol_temp"
-        ).innerHTML = `<strong>${data.current.temp_c}°C</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.temperature}°C</strong>`;
         document.getElementById(
           "kol_humi"
-        ).innerHTML = `<strong>${data.current.humidity}(g/kg)</strong>`;
+        ).innerHTML = `<strong>${data.hourly.relative_humidity_2m[0]}(g/kg)</strong>`;
         document.getElementById(
           "kol_wind"
-        ).innerHTML = `<strong>${data.current.wind_kph}kph</strong>`;
+        ).innerHTML = `<strong>${data.current_weather.windspeed}kph</strong>`;
       })
       .catch((error) => console.error("Error:", error));
 
